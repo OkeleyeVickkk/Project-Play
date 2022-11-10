51dc88973897420b7562c38a9a2779c6e1f5c2f0
Vickkk <justusofnigeria@gmail.com>
2022-11-10 02:25:18 +0100

    Sequence continues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@
 module.exports = {
 	content: ["**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
 	theme: {
-		extend: {},
-		backgroundColor: {
-			// primaryBlue: "#1c64f2",
-			primarySkin: "#23263D",
-			secondarySkin: "#161728",
-			tertiarySkin: "#111827",
+		extend: {
+			backgroundColor: {
+				// primaryBlue: "#1c64f2",
+				primarySkin: "#23263D",
+				secondarySkin: "#161728",
+				tertiarySkin: "#111827",
+			},
 		},
 	},
 	plugins: [require("flowbite/plugin")],
