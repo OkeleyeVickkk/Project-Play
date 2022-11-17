192482b23ce0d6568364098b856b51b8c61229aa
Vickkk <justusofnigeria@gmail.com>
2022-11-17 06:21:12 +0100

    ...

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+// SHAZAM API
+
+// const API_KEY = "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a";
+// const options = {
+// 	method: "GET",
+// 	headers: {
+// 		"X-RapidAPI-Key": `${API_KEY}`,
+// 		"X-RapidAPI-Host": "shazam-song-recognizer.p.rapidapi.com",
+// 	},
+// };
+
+// fetch("https://shazam-song-recognizer.p.rapidapi.com/top_country_tracks?country_code=NG&limit=10&start_from=0", options)
+// 	.then((response) => response.json())
+// 	.then((response) => console.log(response))
+// 	.catch((err) => console.error(err));
