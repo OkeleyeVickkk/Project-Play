0e7a3ad4c3e24cb3767927f3b1388f2dc2832b18
Vickkk <justusofnigeria@gmail.com>
2022-11-21 22:27:36 +0100

    new features and pages

---
+++ src/js/albums.js
@@ -0,0 +1,11 @@
+const splide = new Splide(".splide", {
+	type: "loop",
+	drag: "free",
+	focus: "center",
+	perPage: 3,
+	autoScroll: {
+		speed: -1,
+	},
+});
+
+splide.mount(window.splide.Extensions);
