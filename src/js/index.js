8137a9f457900f1402d148aa0c7a1e11501f104a
Vickkk <justusofnigeria@gmail.com>
2022-11-23 07:28:35 +0100

    New updates

--- src/js/index.js
+++ src/js/index.js
@@ -116,16 +116,3 @@ function DisplaySearchResult() {}
 // 	.then((response) => response.json())
 // 	.then((response) => console.log(response))
 // 	.catch((err) => console.error(err));
-
-const options = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a",
-		"X-RapidAPI-Host": "spotify81.p.rapidapi.com",
-	},
-};
-
-fetch("https://spotify81.p.rapidapi.com/top_200_tracks", options)
-	.then((response) => response.json())
-	.then((response) => console.log(response))
-	.catch((err) => console.error(err));
---
+++ src/js/news.js
@@ -0,0 +1,12 @@
+// const options = {
+// 	method: "GET",
+// 	headers: {
+// 		"X-RapidAPI-Key": "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a",
+// 		"X-RapidAPI-Host": "newscatcher.p.rapidapi.com",
+// 	},
+// };
+
+// fetch("https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=Song&lang=en&sort_by=date&page=1&media=True", options)
+// 	.then((response) => response.json())
+// 	.then((response) => console.log(response))
+// 	.catch((err) => console.error(err));
