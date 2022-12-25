55132775cf7c44efa4987ab5a34995a83cdec6d2
Vickkk <justusofnigeria@gmail.com>
2022-12-25 18:37:36 +0100

    New stuffs

--- src/js/index.js
+++ src/js/index.js
@@ -107,42 +107,87 @@ for (let i = 0; i < 6; i++) {
 // 	return removeZAlphabet;
 // }
 
-function DisplaySearchResult() {}
-// const options = {
-// 	method: "GET",
-// 	headers: {
-// 		"X-RapidAPI-Key": "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a",
-// 		"X-RapidAPI-Host": "musiclinkssapi.p.rapidapi.com",
-// 	},
-// };
-
-// fetch("https://musiclinkssapi.p.rapidapi.com/apiSearch/track?query=Blow%20my%20mind", options)
-// 	.then((response) => response.json())
-// 	.then((response) => console.log(response))
-// 	.catch((err) => console.error(err));
-
-// ? Display Search results
-
+// search result to fetch api data
 const searchBar = document.querySelector(".search-bar-control");
 searchBar.addEventListener("submit", function (e) {
 	e.preventDefault();
 	let inputValue = document.querySelector(".search-bar-control .form-control").value.toLowerCase();
-	const searchString = inputValue.split("by");
-	const songTitle = searchString[0];
-	const songArtists = searchString[1].split("ft")[0];
+
 	const options = {
 		method: "GET",
 		headers: {
-			mode: "no-cors",
 			"X-RapidAPI-Key": `${API_KEY}`,
-			"X-RapidAPI-Host": "powerlyrics.p.rapidapi.com",
+			"X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
 		},
 	};
 
-	fetch(`https://powerlyrics.p.rapidapi.com/getlyricsfromtitleandartist?title=${songTitle}&artist=${songArtists}`, options)
+	fetch(`https://genius-song-lyrics1.p.rapidapi.com/search?q=${inputValue}`, options)
 		.then((response) => response.json())
-		.then((response) => console.log(response))
+		.then((response) => {
+			const resultBoard = document.getElementById("search-result");
+			const otherContent = document.getElementById("sub-content");
+			otherContent.innerHTML = "";
+			resultBoard.innerHTML = "";
+			const results = response.response.hits;
+			results.forEach((result) => {
+				const {
+					artist_names,
+					full_title,
+					id,
+					lyrics_owner_id,
+					path,
+					title,
+					stats: { pageviews },
+				} = result.result;
+				const item = ` 
+			<li class="searched-track">
+				<div class="top">
+					<h2 class="searched-data text-gray-300 font-bold">${full_title}</h2>
+					<div class="mt-4 flex align-center text-sm text-gray-300">
+						Listens: &nbsp;<span class="listened text-sm"> 113k</span>
+					</div>
+				</div>
+				<div class="bottom">
+					<a href="" class="track-element flex items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5"
+							preserveAspectRatio="xMidYMid meet"
+							viewBox="0 0 24 24">
+							<path
+								fill="currentColor"
+								d="M11 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 2a2 2 0 0 0-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2m0 7c1.1 0 2.66.23 4.11.69c-.61.38-1.11.91-1.5 1.54c-.82-.2-1.72-.33-2.61-.33c-2.97 0-6.1 1.46-6.1 2.1v1.1h8.14c.09.7.34 1.34.72 1.9H3v-3c0-2.66 5.33-4 8-4m7.5-3H22v2h-2v5.5a2.5 2.5 0 0 1-2.5 2.5a2.5 2.5 0 0 1-2.5-2.5a2.5 2.5 0 0 1 2.5-2.5c.36 0 .69.07 1 .21V10Z" />
+						</svg>
+						See artist</a
+					>
+					<button class="flex items-center track-element" type="button" data-modal-toggle="track-${id} large-modal">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-5"
+							preserveAspectRatio="xMidYMid meet"
+							viewBox="0 0 24 24">
+							<path fill="currentColor" d="M6 6h7v2H6zm0 6h4v2H6z" />
+							<path
+								fill="currentColor"
+								d="M15 11.97V16H6l-2 2V4h11v2.03c.52-.69 1.2-1.25 2-1.6V4c0-1.1-.9-2-2-2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h9c1.1 0 2-.9 2-2v-2.42a5.16 5.16 0 0 1-2-1.61z" />
+							<path
+								fill="currentColor"
+								d="M6 9h7v2H6zm14-2.82c-.31-.11-.65-.18-1-.18c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3V3h2V1h-4v5.18z" />
+						</svg>
+						See lyrics
+					</button>
+				</div>
+			</li>`;
+				resultBoard.innerHTML += item;
+			});
+			const listItems = resultBoard.querySelectorAll(".searched-track");
+			listItems.forEach((listItem) => {
+				const listItembutton = listItem.querySelector("button.track-element");
+				listItembutton.addEventListener("click", function () {
+					const modal = document.querySelector(".lyrics-modal");
+					console.log(this.getAttribute("data-modal-toggle"));
+				});
+			});
+		})
 		.catch((err) => console.error(err));
 });
-
-// ! api to search for lyrics
