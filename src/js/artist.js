5dee5a8e5a7005fee596aa37bd44c91dea00d22c
Vickkk <justusofnigeria@gmail.com>
2023-01-04 06:13:21 +0100

    60% done

--- src/js/artist.js
+++ src/js/artist.js
@@ -1,5 +1,3 @@
-// get the artist using search params
-
 const API_KEY = "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a";
 
 const currentURL = window.location.search;
@@ -9,25 +7,37 @@ const artist_id = getParams.get("artist_id");
 // const options = {
 // 	method: "GET",
 // 	headers: {
-// 		// "X-RapidAPI-Key": `${API_KEY}`,
+// 		"X-RapidAPI-Key": `${API_KEY}`,
 // 		"X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
 // 	},
 // };
 
 // fetch about artist details himself
-function Boy() {
-	fetch(`https://genius-song-lyrics1.p.rapidapi.com/artists/${artist_id}?text_format=dom`, options)
-		.then((response) => response.json())
-		.then((response) => console.log(response))
-		.catch((err) => console.error(err));
+async function fetchArtistDetails() {
+	const response = await fetch(`https://genius-song-lyrics1.p.rapidapi.com/artists/${artist_id}?text_format=dom`, options);
+	if (!response.ok) throw "Cannot fetch Details";
+	const data = await response.json();
+	data.forEach((result) => {
+		const { description_preview, facebook_name, image_url, instagram_name, header_image_url, name, twitter_name } = result;
 
-	// fetch about artist songs
+		const $albumItemsWrapper = document.querySelector(".recent-albums .albums-wrapper"); //get wrapper for all albums
+		const $albumItem = document.getElementById("album-item"); //get cloneable item
+		let $cloneAlbumItem = $albumItem.content.cloneNode(true);
+
+		//clone the item
+	});
+}
+
+// fetch about artist songs
+async function fetchArtistSongs() {
 	fetch(`https://genius-song-lyrics1.p.rapidapi.com/artists/${artist_id}/songs?sort=popularity`, options)
 		.then((response) => response.json())
 		.then((response) => console.log(response))
 		.catch((err) => console.error(err));
+}
 
-	// fetch about artist albums
+// fetch about artist albums
+async function fetchArtistAlbums() {
 	fetch(`https://genius-song-lyrics1.p.rapidapi.com/artists/${artist_id}/albums`, options)
 		.then((response) => response.json())
 		.then((response) => console.log(response))
--- src/js/news.js
+++ src/js/news.js
@@ -1,126 +1,128 @@
-// const options = {
-// 	method: "GET",
-// 	headers: {
-// 		"X-RapidAPI-Key": "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a",
-// 		"X-RapidAPI-Host": "spotify81.p.rapidapi.com",
-// 	},
-// };
+document.addEventListener("DOMContentLoaded", function () {
+	// const options = {
+	// 	method: "GET",
+	// 	headers: {
+	// 		"X-RapidAPI-Key": "6473c3ce7dmsh28c8afd093343dep1d0f1fjsn02e8bc02b53a",
+	// 		"X-RapidAPI-Host": "spotify81.p.rapidapi.com",
+	// 	},
+	// };
 
-// fetch("https://spotify81.p.rapidapi.com/top_200_tracks", options)
-// 	.then((response) => response.json())
-// 	.then((response) => console.log(response))
-// 	.catch((err) => console.error(err));
+	// fetch("https://spotify81.p.rapidapi.com/top_200_tracks", options)
+	// 	.then((response) => response.json())
+	// 	.then((response) => console.log(response))
+	// 	.catch((err) => console.error(err));
 
-const defaultNewsImage = `https://mir-s3-cdn-cf.behance.net/project_modules/1400/6fe6f228202371.5637141eb4d67.jpg`;
+	const defaultNewsImage = `https://mir-s3-cdn-cf.behance.net/project_modules/1400/6fe6f228202371.5637141eb4d67.jpg`;
 
-// const API_ONE = `6qunVqSv4fjZdS2vf5BTIiVrP5ksWAUvte0jJ6VC`;
-// const URL_ONE = `https://api.thenewsapi.com/v1/news/top?locale=us,ca&language=en&api_token=${API_ONE}`;
+	const API_ONE = `6qunVqSv4fjZdS2vf5BTIiVrP5ksWAUvte0jJ6VC`;
+	const URL_ONE = `https://api.thenewsapi.com/v1/news/top?locale=us,ca&language=en&api_token=${API_ONE}`;
 
-const OPTIONS_ONE = {
-	method: "GET",
-	header: {
-		"Content-Type": "application/json",
-	},
-};
+	const OPTIONS_ONE = {
+		method: "GET",
+		header: {
+			"Content-Type": "application/json",
+		},
+	};
 
-fetch(URL_ONE, OPTIONS_ONE)
-	.then((response) => response.json())
-	.then((results) => {
-		console.log(results);
-		pasteSectionOneResults(results.data);
-	})
-	.catch((error) => console.log(error));
+	fetch(URL_ONE, OPTIONS_ONE)
+		.then((response) => response.json())
+		.then((results) => {
+			console.log(results);
+			pasteSectionOneResults(results.data);
+		})
+		.catch((error) => console.log(error));
 
-function pasteSectionOneResults(results) {
-	//main function one
-	const sectionOne = document.querySelector("section.first");
-	const sectionOneMainTemplate = document.getElementById("main-news-template");
-	const sectionOneMain = sectionOne.querySelector(".main-news");
-	const subNewsTemplate = document.getElementById("sub-template");
-	const sectionOneSub = document.querySelector(".sub-news-inner");
+	function pasteSectionOneResults(results) {
+		//main function one
+		const sectionOne = document.querySelector("section.first");
+		const sectionOneMainTemplate = document.getElementById("main-news-template");
+		const sectionOneMain = sectionOne.querySelector(".main-news");
+		const subNewsTemplate = document.getElementById("sub-template");
+		const sectionOneSub = document.querySelector(".sub-news-inner");
 
-	const main = results.slice(0, 3);
-	const { title, url, image_url, description, source, published_at } = main[0];
-	const date = filterDate(published_at);
-
-	const MainTemplate = sectionOneMainTemplate.content.cloneNode(true);
-
-	MainTemplate.querySelector("a.news-link").href = url;
-	MainTemplate.querySelector(".image-wrapper img.img-fluid").src = image_url ?? defaultNewsImage;
-	MainTemplate.querySelector(".date small").textContent = date;
-	MainTemplate.querySelector(".author small").textContent = `Source - ${source ?? "Unknown"}`;
-	MainTemplate.querySelector(".middle .news-title").textContent = title;
-	MainTemplate.querySelector(".bottom .news-content").textContent = description;
-
-	sectionOneMain.appendChild(MainTemplate); //add to the largest section
-
-	results.slice(1, 3).forEach((result) => {
-		const { title, url, image_url, description, source, published_at } = result;
+		const main = results.slice(0, 3);
+		const { title, url, image_url, description, source, published_at } = main[0];
 		const date = filterDate(published_at);
 
-		const subTemplate = subNewsTemplate.content.cloneNode(true);
+		const MainTemplate = sectionOneMainTemplate.content.cloneNode(true);
 
-		subTemplate.querySelector("a.news-link").href = url;
-		subTemplate.querySelector(".image-wrapper img.img-fluid").src = image_url ?? defaultNewsImage;
-		subTemplate.querySelector(".date small").textContent = date;
-		subTemplate.querySelector(".author small").textContent = `Source - ${source ?? "Unknown"}`;
-		subTemplate.querySelector(".middle .news-title").textContent = title;
-		subTemplate.querySelector(".bottom .news-content").innerHTML = description;
+		MainTemplate.querySelector("a.news-link").href = url;
+		MainTemplate.querySelector(".image-wrapper img.img-fluid").src = image_url ?? defaultNewsImage;
+		MainTemplate.querySelector(".date small").textContent = date;
+		MainTemplate.querySelector(".author small").textContent = `Source - ${source ?? "Unknown"}`;
+		MainTemplate.querySelector(".middle .news-title").textContent = title;
+		MainTemplate.querySelector(".bottom .news-content").textContent = description;
 
-		sectionOneSub.appendChild(subTemplate); //add to screen
-	});
-}
+		sectionOneMain.appendChild(MainTemplate); //add to the largest section
 
-// const API_TWO = `40fff40b72b4dae0fc8cc694ef057ab4`;
-const URL_TWO = `https://gnews.io/api/v4/top-headlines?token=${API_TWO}&topic=breaking-news&lang=en`;
+		results.slice(1, 3).forEach((result) => {
+			const { title, url, image_url, description, source, published_at } = result;
+			const date = filterDate(published_at);
 
-// const options = {
-// 	method: "GET",
-// 	header: {
-// 		"Content-Type": "application/json",
-// 	},
-// };
+			const subTemplate = subNewsTemplate.content.cloneNode(true);
 
-fetch(URL_TWO, options)
-	.then((response) => response.json())
-	.then((results) => {
-		pasteSectionTwoResults(results.articles);
-	})
-	.catch((error) => console.log(error));
+			subTemplate.querySelector("a.news-link").href = url;
+			subTemplate.querySelector(".image-wrapper img.img-fluid").src = image_url ?? defaultNewsImage;
+			subTemplate.querySelector(".date small").textContent = date;
+			subTemplate.querySelector(".author small").textContent = `Source - ${source ?? "Unknown"}`;
+			subTemplate.querySelector(".middle .news-title").textContent = title;
+			subTemplate.querySelector(".bottom .news-content").innerHTML = description;
 
-function pasteSectionTwoResults(results) {
-	//main function two
-	const sectionTwo = document.querySelector("section.second");
-	const sectionTwoArticlesWrapper = sectionTwo.querySelector("ul.articles-wrapper");
-	const sectionTwoTemplate = document.getElementById("sectionTwoTemplate");
+			sectionOneSub.appendChild(subTemplate); //add to screen
+		});
+	}
 
-	results.forEach((result) => {
-		const { title, url, image, content, publishedAt, source = { name, url } } = result;
-		const date = filterDate(publishedAt);
+	const API_TWO = `40fff40b72b4dae0fc8cc694ef057ab4`;
+	const URL_TWO = `https://gnews.io/api/v4/top-headlines?token=${API_TWO}&topic=breaking-news&lang=en`;
 
-		const clonedTemplate = sectionTwoTemplate.content.cloneNode(true);
+	const OPTIONS_TWO = {
+		method: "GET",
+		header: {
+			"Content-Type": "application/json",
+		},
+	};
 
-		clonedTemplate.querySelector(".article-item .article-link").href = url;
-		clonedTemplate.querySelector(".article-image-wrapper img.img-fluid").src = image ?? defaultNewsImage;
-		clonedTemplate.querySelector("small.article-author").innerHTML = `- ${source.name ?? "Unknown"}`;
-		clonedTemplate.querySelector("small.date").innerHTML = date;
-		clonedTemplate.querySelector("h3.article-title").innerHTML = title;
-		clonedTemplate.querySelector(".source small").innerHTML = `Source - ${source.url}`;
-		clonedTemplate.querySelector("small.article-writeup").innerHTML = content;
+	fetch(URL_TWO, OPTIONS_TWO)
+		.then((response) => response.json())
+		.then((results) => {
+			pasteSectionTwoResults(results.articles);
+		})
+		.catch((error) => console.log(error));
 
-		sectionTwoArticlesWrapper.appendChild(clonedTemplate); // add to screen
-	});
-}
+	function pasteSectionTwoResults(results) {
+		//main function two
+		const sectionTwo = document.querySelector("section.second");
+		const sectionTwoArticlesWrapper = sectionTwo.querySelector("ul.articles-wrapper");
+		const sectionTwoTemplate = document.getElementById("sectionTwoTemplate");
 
-// secondary functions
-function filterDate(publishedAt) {
-	const oddDate = publishedAt.split("T")[0].split("-");
-	const year = oddDate[0],
-		month = oddDate[1],
-		day = oddDate[2];
-	return `${day}-${month}-${year}`;
-}
+		results.forEach((result) => {
+			const { title, url, image, content, publishedAt, source = { name, url } } = result;
+			const date = filterDate(publishedAt);
 
-function filterAuthor(author) {
-	return author.split("https://")[0];
-}
+			const clonedTemplate = sectionTwoTemplate.content.cloneNode(true);
+
+			clonedTemplate.querySelector(".article-item .article-link").href = url;
+			clonedTemplate.querySelector(".article-image-wrapper img.img-fluid").src = image ?? defaultNewsImage;
+			clonedTemplate.querySelector("small.article-author").innerHTML = `- ${source.name ?? "Unknown"}`;
+			clonedTemplate.querySelector("small.date").innerHTML = date;
+			clonedTemplate.querySelector("h3.article-title").innerHTML = title;
+			clonedTemplate.querySelector(".source small").innerHTML = `Source - ${source.url}`;
+			clonedTemplate.querySelector("small.article-writeup").innerHTML = content;
+
+			sectionTwoArticlesWrapper.appendChild(clonedTemplate); // add to screen
+		});
+	}
+
+	// secondary functions
+	function filterDate(publishedAt) {
+		const oddDate = publishedAt.split("T")[0].split("-");
+		const year = oddDate[0],
+			month = oddDate[1],
+			day = oddDate[2];
+		return `${day}-${month}-${year}`;
+	}
+
+	function filterAuthor(author) {
+		return author.split("https://")[0];
+	}
+});
