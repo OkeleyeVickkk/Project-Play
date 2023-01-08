e591fe32a86696fab18d3a8a4082a186e3bb0ba8
Vickkk <justusofnigeria@gmail.com>
2023-01-08 04:45:26 +0100

    updates and new bugs

--- src/js/play-musics.js
+++ src/js/play-musics.js
@@ -6,8 +6,6 @@ window.addEventListener("DOMContentLoaded", function () {
 	const stop_button = document.getElementById("stop");
 	const audio_element = document.getElementById("audio");
 
-	console.log(playPause_button);
-
 	// song doms
 	const artists = document.querySelector(".song-artist h5");
 	const song_title = document.querySelector(".song-title h3");
@@ -17,34 +15,39 @@ window.addEventListener("DOMContentLoaded", function () {
 
 	const songs = [
 		{
-			song_path: "/src/folder/Rag n Bone Man - Human (Official Video).mp3",
-			artist: "Rag n Bone Man",
-			title: "Human",
-			coverPhoto: "https://static.billboard.com/files/media/Rag-n-Bone-Man-Human-vid-still-2017-billboard-1548-compressed.jpg",
-		},
-		{
-			song_path: "/src/folder/MichaelJackson-BillieJean.mp3",
-			artist: "Michael Jackson",
-			title: "Billie Jean",
+			song_path: "/src/folder/Peace Be Unto You (PBUY).mp3",
+			artist: "Asake",
+			title: "PBUY",
+			coverPhoto: "https://trendybeatz.com/images/Asake-Peace-Be-Unto-You-PBUY-Artwork.jpeg",
 		},
 		{
 			song_path: "/src/folder/Passenger Coins In A Fountain (Official Album Audio).mp3",
 			artist: "Passenger",
 			title: "Coins In A Fountain",
-		},
-		{
-			song_path: "/src/folder/Peace Be Unto You (PBUY).mp3",
-			artist: "Asake",
-			title: "PBUY",
+			coverPhoto: "https://i.ytimg.com/vi/-yDWjtrgkb0/maxresdefault.jpg",
 		},
 		{
 			song_path: "/src/folder/Shakira - Hips Don t Lie (Official 4K Video) ft. Wyclef Jean.mp3",
 			artist: "Shakira Ft Wyclef Jean",
 			title: "Hips Don't Lie",
+			coverPhoto: "https://4.bp.blogspot.com/-QbxSRkaaYhc/UVcHk_uaeEI/AAAAAAAALw8/081rF0_Q9P4/s1600/ShakiraHipsDontLie.jpg",
+		},
+		{
+			song_path: "/src/folder/MichaelJackson-BillieJean.mp3",
+			artist: "Michael Jackson",
+			title: "Billie Jean",
+			coverPhoto: "https://i.ytimg.com/vi/OuGUJrfdpdM/maxresdefault.jpg",
+		},
+		{
+			song_path: "/src/folder/Rag n Bone Man - Human (Official Video).mp3",
+			artist: "Rag n Bone Man",
+			title: "Human",
+			coverPhoto: "https://static.billboard.com/files/media/Rag-n-Bone-Man-Human-vid-still-2017-billboard-1548-compressed.jpg",
 		},
 	];
 
 	playPause_button.addEventListener("click", playPause);
+	stop_button.addEventListener("click", stopPlayer);
 	initializePlay(); //default when the page loads
 
 	function initializePlay() {
@@ -63,11 +66,20 @@ window.addEventListener("DOMContentLoaded", function () {
 		audio_element.src = song.song_path;
 	}
 
-	function playPause() {
+	function playPause(e) {
+		e.stopPropagation();
 		if (audio_element.pause) {
 			audio_element.play();
 		} else {
-			audio_element.play();
+			audio_element.pause();
+		}
+		// audio_element.pause ? audio_element.play() : audio_element.pause();
+	}
+
+	function stopPlayer() {
+		if (audio_element.pause || audio_element.play) {
+			audio_element.pause();
+			audio_element.currentTime = 0;
 		}
 	}
 });
